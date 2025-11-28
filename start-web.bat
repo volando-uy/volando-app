ee52104eb3bd670e3ee58cc99a3f635eb40dd40a
gyabisito <josecitohernandez5@gmail.com>
2025-11-28 17:52:34 -0300

    mejora bat

--- start-web.bat
+++ start-web.bat
@@ -2,12 +2,18 @@
 setlocal ENABLEDELAYEDEXPANSION
 title VolandoUY - Web Server
 
+:: Configuración
 set CONFIG_FILE=%USERPROFILE%\volandouy\application.properties
+set WAR_NAME=app-web-jsp.war
+set WAR_PATH=app-web\target\%WAR_NAME%
+set TOMCAT_DIR=tomcat
+set TOMCAT_BIN=%TOMCAT_DIR%\bin
+set TOMCAT_WEBAPPS=%TOMCAT_DIR%\webapps
+set SERVER_XML=%TOMCAT_DIR%\conf\server.xml
 set PORT=
-set /a RMI_START=8205
 
-echo Cargando configuración desde: %CONFIG_FILE%
-
+:: Leer puerto desde application.properties
+echo Leyendo configuración desde: %CONFIG_FILE%
 if not exist "%CONFIG_FILE%" (
     echo ERROR: No se encontró %CONFIG_FILE%
     pause
@@ -19,30 +25,52 @@ for /f "tokens=1,2 delims==" %%A in (%CONFIG_FILE%) do (
 )
 
 if "%PORT%"=="" (
-    echo ERROR: No se encontró cargo.port en el archivo.
+    echo ERROR: No se encontró cargo.port en el archivo
     pause
     exit /b 1
 )
 
-echo Puerto HTTP: %PORT%
+echo Puerto configurado: %PORT%
 
-:: Buscar RMI libre
-set RMI=%RMI_START%
-:find_rmi
-netstat -ano | findstr :!RMI! >nul
-if not errorlevel 1 (
-    set /a RMI+=1
-    goto find_rmi
+:: Verificar si el WAR existe
+if not exist "%WAR_PATH%" (
+    echo No se encontró %WAR_NAME%, compilando proyecto...
+    pushd app-web
+    mvn clean package -DskipTests
+    popd
+
+    if not exist "%WAR_PATH%" (
+        echo ERROR: La compilación falló o el WAR aún no se generó.
+        pause
+        exit /b 1
+    )
 )
 
-echo Puerto RMI disponible: %RMI%
+:: Copiar WAR al webapps de Tomcat
+echo Copiando WAR a Tomcat...
+copy /Y "%WAR_PATH%" "%TOMCAT_WEBAPPS%\%WAR_NAME%" >nul
 
-cd /d "%~dp0app-web"
+:: Actualizar puerto en server.xml si es necesario
+echo Configurando Tomcat en el puerto %PORT%...
+powershell -Command ^
+    "(Get-Content '%SERVER_XML%') -replace '(<Connector port=\")\d+(\" protocol=\"HTTP/1.1\")', '${1}%PORT%${2}' | Set-Content '%SERVER_XML%'"
 
-:: Ejecutar directamente Maven. El script espera aquí hasta que termine.
-echo Iniciando servidor... (Ctrl+C para detenerlo)
-mvn cargo:run -Dcargo.servlet.port=%PORT% -Dcargo.rmi.port=%RMI%
+:: Iniciar Tomcat
+echo Iniciando Tomcat...
+pushd "%TOMCAT_BIN%"
+call startup.bat
+popd
 
-echo Servidor detenido correctamente.
-pause
+echo.
+echo Aplicación desplegada en: http://localhost:%PORT%/%WAR_NAME:~0,-4%/
+echo Presiona una tecla para detener Tomcat...
+pause >nul
 
+:: Apagar Tomcat
+echo Deteniendo Tomcat...
+pushd "%TOMCAT_BIN%"
+call shutdown.bat
+popd
+
+echo Tomcat detenido.
+timeout /t 1 >nul
